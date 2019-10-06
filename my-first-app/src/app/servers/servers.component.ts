d37803b845a5be446cbe7981cf1bae507efb58df
Tshifhiwa <tdmadima@gmail.com>
2019-10-06 18:43:09 +0200

    task

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-servers></app-servers><app-servers></app-servers>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
